2376157361f829acaaf8331e41143300eca973fa
MikeOxMedium <111542535+MikeOxMedium@users.noreply.github.com>
2023-05-23 13:30:01 +1200

    Add files via upload

---
+++ 03_Assesment/02_Instructions.py
@@ -0,0 +1,23 @@
+import random
+
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            return "yes"
+
+        elif response == "no" or response == "n":
+            return "no"
+
+        else:
+            print("Please answer yes / no")
+
+
+def instructions():
+    print("**** How to Play ****")
+    print()
+    print("The rules of the game go here")
+    print()
+    return ""
