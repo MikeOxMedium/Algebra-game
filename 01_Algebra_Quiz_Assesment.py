342a8cda383a30f0762fa9cd9ab9d53ca2281ebf
MikeOxMedium <111542535+MikeOxMedium@users.noreply.github.com>
2023-06-15 11:04:36 +1200

    Add files via upload

--- 01_Algebra_Quiz_Assesment.py
+++ 01_Algebra_Quiz_Assesment.py
@@ -53,17 +53,6 @@ def statement_generator(statement, decoration):
     return ""
 
 
-statement_generator("Welcome to The Algebra Quiz", "*")
-print()
-
-# Main Routine goes here...
-played_before = yes_no("Have you played this game before? ")
-
-if played_before == "no":
-    instructions()
-
-
-
 def check_rounds():
     while True:
         response = input("How many rounds? or press <Enter> for continuous mode: ")
@@ -84,31 +73,65 @@ def check_rounds():
 
         return response
 
+
+statement_generator("Welcome to The Algebra Quiz", "*")
+print()
+
+# Main Routine goes here...
+
+played_before = yes_no("Have you played this game before? ")
+
+if played_before == "no":
+        instructions()
+
+
+# List of valid response
+
+
 rounds_played = 0
 rounds_lost = 0
 rounds_drawn = 0
 
 rounds = check_rounds()
 if rounds == "":
-        mode = "infinite"
-rounds = 5
+    mode = "infinite"
+    rounds = 5
 
 while rounds_played < rounds:
 
-print()
-if mode == "infinite":
-    heading = "Continuous Mode: " \
-              "Round {}".format(rounds_played + 1)
-    rounds += 1
+    print()
+    if mode == "infinite":
+        heading = "Continuous Mode: " \
+                  "Round {}".format(rounds_played + 1)
+        rounds += 1
 
-else:
-    heading = "Round {} of " \
-              "{}".format(rounds_played + 1, rounds)
+    else:
+        heading = "Round {} of " \
+                  "{}".format(rounds_played + 1, rounds)
 
+    print(heading)
+    rounds_played += 1
 
+    end_game = "no"
+    while end_game == "no":
 
+        if mode == "infinite":
+            heading = f"Round {rounds_played + 1} (infinite mode)"
+            rounds += 1
+        else:
+            heading = f"Round {rounds_played + 1} of {rounds}"
 
+        print(heading)
 
+        rounds_played += 1
+
+        answer = input("press enter")
+
+        # check if we are out of rounds
+        if rounds_played >= rounds:
+            break
+        elif answer == "xxx":
+            break
 
 
 
